4972bc2579460ac56d56e66dde61e1a0dbb5b8d2
Isabel <isabel.kavalco@gmail.com>
2023-02-02 18:39:15 -0300

    first script

--- scripts/hjashjk.gd
+++ scripts/hjashjk.gd
@@ -1,18 +1,6 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	self.position = Vector2(100,100)
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-# aqui
+func _process(_delta):
+	self.position = self.position + Vector2(1,1)
