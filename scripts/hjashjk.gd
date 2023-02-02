8de6fcada403cd88c57e3f19e13bd6ef4eaf3334
PRP <prpsuper@gmail.com>
2023-02-02 18:33:14 -0300

    transform test

--- scripts/hjashjk.gd
+++ scripts/hjashjk.gd
@@ -8,9 +8,11 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	self.position = Vector2(100,100)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+# aqui
